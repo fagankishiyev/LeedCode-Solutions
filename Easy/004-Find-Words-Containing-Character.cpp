4e1ea8cc4f14bf17616429d99fc7888fb6866012
Fagan Kishiyev <fagankishiyev@gmail.com>
2026-06-08 21:56:11 +0400

    Find Words Containing Character

--- Easy/004-Find-Words-Containing-Character.cpp
+++ Easy/004-Find-Words-Containing-Character.cpp
@@ -13,20 +13,29 @@ using namespace std;
 class Solution {
 public:
     vector<int> findWordsContaining(vector<string>& words, char x) {
-        vector<int> result;
-        for (int i = 0; i < words.size(); i++) {
-            if (words[i].find(x) != string::npos) {
-                result.push_back(i);
+        vector<int>index;
+        for (int i=0;i<words.size();i++){
+            for(int j=0;j<words[i].size();j++){
+                if(words[i][j]==x){
+                    index.push_back(i);
+                    break;
+                }
             }
         }
-        return (num2-num1);
+        return index;
+        
     }
 };
 
 int main() {
     Solution s1;
-    int n = 10, m = 3;
-    int result = s1.differenceOfSums(n, m);
-    cout << "Difference: " << result << endl;
+    vector<string> words = {"leet","code"};
+    char x = 'e';
+    vector<int> result = s1.findWordsContaining(words, x);
+    cout << "Indices: ";
+    for (int i : result) {
+        cout << i << " ";
+    }
+    cout << endl;
     return 0;
 }
