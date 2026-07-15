161d4a8a4d2dc3fe41c973e47b1d3c615b6e515b
Fagan Kishiyev <fagankishiyev@gmail.com>
2026-07-15 16:33:32 +0400

    Concatenate Array With Reverse

---
+++ Easy/012-Jewels and Stones.cpp
@@ -0,0 +1,31 @@
+/*
+LeetCode #12 - Concatenate Array With Reverse
+link:https://leetcode.com/problems/jewels-and-stones/description/
+
+
+Input: jewels = "aA", stones = "aAAbbbb"
+Output: 3
+*/
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    int numJewelsInStones(string jewels, string stones) {
+        int count=0;
+        for(int i=0;i<stones.size();i++)
+        {
+            for(int j=0;j<jewels.size();j++)
+            {
+                if(stones[i]==jewels[j])
+                {
+                    count++;
+                    break;
+                }
+                
+            }
+        }
+        return count;
+        
+    }
+};
