f805377dfe30e60639e03c52f5b81e20ca029b0d
Fagan Kishiyev <fagankishiyev@gmail.com>
2026-08-11 23:56:50 +0400

    Remove Element

---
+++ Easy/025-Remove-Element.cpp
@@ -0,0 +1,29 @@
+/*
+LeetCode #24 - Remove Element
+link:https://leetcode.com/problems/remove-element/description/
+
+Input: nums = [3,2,2,3], val = 3
+Output: 2, nums = [2,2,_,_]
+Explanation: Your function should return k = 2, with the first two elements of nums being 2.
+It does not matter what you leave beyond the returned k (hence they are underscores).
+*/
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int k = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        return k;
+    }
+};
